5def47eae8e604775abae165cabee3a0068222bf
LiQiming <leeqiming@163.com>
2022-06-28 14:56:33 +0800

    define beans

--- src/main/java/com/example/spring/batch/demo/bean/DbData.java
+++ src/main/java/com/example/spring/batch/demo/bean/DbData.java
@@ -1,8 +1,10 @@
 package com.example.spring.batch.demo.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DbData {
     private String customerId;
     private String userId;
--- src/main/java/com/example/spring/batch/demo/process/BatchConfig.java
+++ src/main/java/com/example/spring/batch/demo/process/BatchConfig.java
@@ -1,10 +1,19 @@
 package com.example.spring.batch.demo.process;
 
+import com.example.spring.batch.demo.bean.DbData;
+import com.example.spring.batch.demo.bean.MongoData;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.batch.item.support.ListItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableBatchProcessing
@@ -21,5 +30,26 @@ public class BatchConfig {
     public void setStepBuilderFactory(
             StepBuilderFactory stepBuilderFactory) {
         this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public ItemReader<DbData> reader() {
+        Iterator<DbData> iterator =
+                Stream.generate(() -> new DbData("cnhsbcliiqming", "userId", "entitle", "description"))
+                        .limit(10)
+                        .iterator();
+        IteratorItemReader<DbData> reader = new IteratorItemReader<>(iterator);
+        return reader;
+    }
+
+    @Bean
+    public DataTransform process() {
+        return new DataTransform();
+    }
+
+    @Bean
+    public ListItemWriter<MongoData> writer(MongoData data) {
+//        System.out.println(mongoData);
+        return new ListItemWriter<MongoData>();
     }
 }
